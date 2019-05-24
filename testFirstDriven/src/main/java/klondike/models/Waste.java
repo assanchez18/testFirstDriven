3efecd2beb11813c5e065a201032a06e7af336c2
Sergio Sanchez <assanchez18@gmail.com>
2019-05-24 19:15:38 +0200

    Removes inheritance of CardStack from Waste

--- testFirstDriven/src/main/java/klondike/models/Waste.java
+++ testFirstDriven/src/main/java/klondike/models/Waste.java
@@ -1,4 +1,31 @@
 package klondike.models;
 
-public class Waste extends CardStack {
+import java.util.Stack;
+
+public class Waste {
+	
+	private Stack<Card> cards;
+	
+	public Waste() {
+		this.cards = new Stack<Card>();
+	}
+	
+	public boolean empty() {
+		return this.cards.empty();
+	}
+	
+	public Card peek() {
+		return this.cards.peek();
+	}
+	
+	public Card pop() {
+		return cards.pop();
+	}
+	
+	public void push(Card card) {
+		assert card != null;
+		this.cards.push(card);
+	}
+	
+	
 }
